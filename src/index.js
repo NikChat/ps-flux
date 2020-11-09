c0318e56cc03b7e6a88820757fdf202a6f5f4994
Nikolaos Chatzitheodorou <chatzitheodorou@intralot.com>
2020-11-09 13:43:41 +0200

    First Components, Simple routing

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { render } from "react-dom"; // creates a const called render that references react-dom's render function
+import App from "./components/App";
+
+render(<App />, document.getElementById("root"));
