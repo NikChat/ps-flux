7a41519ab29a11860ce7bbe8f62a45ffead7616a
Nikolaos Chatzitheodorou <chatzitheodorou@intralot.com>
2020-11-10 16:13:01 +0200

    Forms: Course Controlled Form

--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,13 +1,38 @@
-import React from "react";
-// import { Prompt } from "react-router-dom"
+import React, { useState } from "react";
+import CourseForm from "./CourseForm";
+import * as courseApi from "../api/courseApi";
 
 const ManageCoursePage = props => {
-  // debugger;
+  const [course, setCourse] = useState({
+    id: null,
+    slug: "",
+    title: "",
+    authorId: null,
+    category: ""
+  });
+
+  function handleChange({ target }) { // event.target
+    setCourse({
+      ...course,
+      [target.name]: target.value
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    courseApi.saveCourse(course).then(() => {
+      props.history.push("/courses"); // Programmatic Redirect to the CoursesPage with React Router (instead of <Redirect>)
+    });
+  }
+
   return (
     <>
       <h2>Manage Course</h2>
-      {/* <Prompt when={true} message="Are you sure you want to leave?" /> */}
-      {props.match.params.slug}
+      <CourseForm
+        course={course}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+      />
     </>
   );
 };
