eec4b6b4c423c7710a28c5af0e6658cdd44f09fb
Nikolaos Chatzitheodorou <chatzitheodorou@intralot.com>
2020-11-10 12:04:09 +0200

    React Router (Routes, Link, NavLink, Redirect, Switch-404s)

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { render } from "react-dom"; // creates a const called render that references react-dom's render function
 import App from "./components/App";
+import { BrowserRouter as Router } from "react-router-dom";
 
-render(<App />, document.getElementById("root"));
+render(
+    <Router>
+      <App />
+    </Router>,
+    document.getElementById("root")
+  );
